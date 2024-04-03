abf87614e63bd4895eb0bcbadc31dd58bbe10f3a
deepika621 <18311a12l2@sreenidhi.edu.in>
2024-04-03 18:02:39 +0530

    react-router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import AppContext from './context';
+
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
+import Cart from './pages/Cart/Cart';
+import { useState } from 'react';
+
 function App() {
+
+  // 2. Initialize Store.
+  const [cartItems, setCartItems]=useState([]);
+
+  // 3. Dispatchers.
+  const dispatcherEvents = (actionType, payload)=>{
+      switch(actionType){
+        case "ADD_ITEM":{
+          console.log(payload);
+          let items = cartItems.slice();
+          items.push(payload);
+          setCartItems(items);
+          console.log(items);
+          break;
+        }
+        case "UPDATE_ITEM":{
+          let items = cartItems.slice();
+          let index = items.findIndex(p=> p.id===payload.id);
+          items[index]=payload;
+          setCartItems(items);
+          break;
+        }
+        case "DELETE_ITEM":{
+          let items = [...cartItems];
+          let index = items.findIndex(p=> p.id===payload);
+          items.splice(index, 1);
+          setCartItems(items);
+          break;
+        }
+        default:{
+          console.log("INVALID...");
+        }
+      }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // 4. Initializing Context.
+    <AppContext.Provider value={{cartItems, dispatcherEvents}}>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='register' element={<Register />}></Route>
+      <Route path='login' element={<Login />}></Route>
+      <Route path='cart' element={<Cart />}></Route>
+    </Routes>
+  </BrowserRouter>
+  </AppContext.Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './pages/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+   
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Register from './pages/Register/Register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element= {<Home/>}></Route>
+      <Route path='/register' element= {<Register/>}></Route>\
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
